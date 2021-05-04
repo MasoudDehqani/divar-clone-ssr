e28a939b66999ad834047b753a100a3ecb51f3e0
Masoud <masoud67.md@gmail.com>
2021-05-04 19:41:24 +0430

    fix: cookie bug fixed

--- src/components/context/cookieHandle.ts
+++ src/components/context/cookieHandle.ts
@@ -5,13 +5,13 @@ export default function cookieHandle(city: string | string[]) {
 
     if (city) {
       localStorage.setItem("city", city as string)
-      document.cookie = `city=${city}`
+      document.cookie = `city=${city}; max-age=31536000`
       return city
     }
 
     city = localStorage.getItem("city")
     console.log(city)
-    if (city) document.cookie = `city=${city}`
+    if (city) document.cookie = `city=${city}; max-age=31536000`
     return city
   }
 }
